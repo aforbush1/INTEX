fa3b0fa102d8f3932539c68223981799f5a17952
aforbush1 <aforbush17@gmail.com>
2023-12-04 10:14:11 -0700

    commit

--- index.js
+++ index.js
@@ -8,26 +8,26 @@ const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended:true}));
 
-const knex = require("knex")({
-    client: "pg",
-    connection: {
-    host: process.env.RDS_HOSTNAME || "localhost",
-    user: process.env.RDS_USERNAME || "postgres",
-    password: process.env.RDS_PASSWORD || "",
-    database: process.env.RDS_DB_NAME || "",
-    port: process.env.RDS_PORT || 5432
-    }
-});
+// const knex = require("knex")({
+//     client: "pg",
+//     connection: {
+//     host: process.env.RDS_HOSTNAME || "localhost",
+//     user: process.env.RDS_USERNAME || "postgres",
+//     password: process.env.RDS_PASSWORD || "",
+//     database: process.env.RDS_DB_NAME || "",
+//     port: process.env.RDS_PORT || 5432
+//     }
+// });
 
-app.get("/", (req, res) => { 
-  res.send("Hello World")
-//   knex.select().from("country").then((country) => {
-//     res.render("displayCountry", { mycountry: country });
-//   });
-});
+// app.get("/", (req, res) => { 
+//   res.send("Hello World")
+// //   knex.select().from("country").then((country) => {
+// //     res.render("displayCountry", { mycountry: country });
+// //   });
+// });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"))
+  res.sendFile(path.join(__dirname, "views/index.ejs"))
 })
 
 app.listen(port, () => console.log("Server is listening"));
