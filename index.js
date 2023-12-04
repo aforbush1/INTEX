4231223879400f013adc5c74a388fd953f57fe08
aforbush1 <aforbush17@gmail.com>
2023-12-04 09:41:52 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// Author: Adam Forbush, Josh Thatcher, Chris Fowler, Ryan Hawkins
+// Description: Index.js File
+
+const express = require ("express");
+let app = express();
+let path = require("path");
+const port = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended:true}));
+
+const knex = require("knex")({
+    client: "pg",
+    connection: {
+    host: process.env.RDS_HOSTNAME || "localhost",
+    user: process.env.RDS_USERNAME || "postgres",
+    password: process.env.RDS_PASSWORD || "",
+    database: process.env.RDS_DB_NAME || "",
+    port: process.env.RDS_PORT || 5432
+    }
+});
+
+app.get("/", (req, res) => { 
+  res.send("Hello World")
+//   knex.select().from("country").then((country) => {
+//     res.render("displayCountry", { mycountry: country });
+//   });
+});
+
+app.listen(port, () => console.log("Server is listening"));
