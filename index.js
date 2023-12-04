4699ec6945336285536611f911207fe9b1825605
Joshua Thatcher <joshuagthatcher@gmail.com>
2023-12-04 14:21:48 -0700

    joshua

--- index.js
+++ index.js
@@ -29,10 +29,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"))
 })
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/index.html"))
-})
-
 app.get("/login", (req, res) => {
   res.sendFile(path.join(__dirname, "views/login.html"))
 })
