98bdec75ea2bde3457d690fb91c63c6e1fa9c299
Joshua Thatcher <joshuagthatcher@gmail.com>
2023-12-04 17:15:30 -0700

    joshua

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.get("/surveyForm", (req, res) => {
     res.sendFile(path.join(__dirname, "views/surveyForm.html"))
 })
 
-app.post()
+//app.post()
 
 
 app.listen(port, () => console.log("Server is listening"));
