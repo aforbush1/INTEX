86ad4b4c731abd210e40e48e1664dc4c4cb3e395
Joshua Thatcher <joshuagthatcher@gmail.com>
2023-12-05 14:58:01 -0700

    joshua thatcher adding viewuser and route

--- index.js
+++ index.js
@@ -50,7 +50,8 @@ app.post("/submitLogin", (req, res) => {
 
 app.post("/createUser", (res,res) =>{
 
-    knex("loginInfo").insert({username: req.body.})
+    knex("loginInfo").insert({username: req.body.username, password : req.body.password , firstName :req.body.fName , lastName : req.body.lName, email: req.body.email}).then
+    (res.render("/viewUser", {username: req.body.username, password : req.body.password , firstName :req.body.fName , lastName : req.body.lName, email: req.body.email}))
 })
 
 app.get("/createAccount", (req, res) => {
