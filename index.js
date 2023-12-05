25e1934ede5f74b7786f9bdfb7eeded0807ee745
rbhawkins999 <rbhawkins999@gmail.com>
2023-12-05 10:40:40 -0700

    change small thing

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const knex = require("knex")({
     connection: {
         host: process.env.RDS_HOSTNAME || "localhost",
         user: process.env.USER_NAME || "postgres",
-        password: process.env.RDS_PASSWORD || "rhawk999",
+        password: process.env.RDS_PASSWORD || "***",
         database: process.env.RDS_DB_NAME || "plainsville",
         port: process.env.RDS_PORT || 5432,
         // ssl: process.env.DB_SSL ? {rejectUnauthorized:false}:false
