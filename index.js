b6758bd092776cf611cfab05eba019fed702a8ac
rbhawkins999 <rbhawkins999@gmail.com>
2023-12-06 21:51:36 -0700

    back

--- index.js
+++ index.js
@@ -228,12 +228,19 @@ app.post("/deleteUser/:id", (req, res) => {
 app.get("/dashboard", (req, res) =>
     res.send("<div class='tableauPlaceholder' id='viz1701795084373' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Bo&#47;Book1_17011425626370&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Book1_17011425626370&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Bo&#47;Book1_17011425626370&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1701795084373');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1366px';vizElement.style.height='795px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1366px';vizElement.style.height='795px';} else { vizElement.style.width='100%';vizElement.style.height='1127px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>"));
 
-app.get("/userProfile", (req, res) => {
-    // Retrieve the user data using Knex.js
-    knex.select().from("loginInfo").then((loginInfo) => {
-        res.render("userProfile", { theLogin: loginInfo }); // Pass 'theLogin' as an object property
+app.get("/userprofile/:id", (req, res) => {
+    const userId = req.params.id;
+
+    // Retrieve user data based on userId using Knex.js (this is just an example)
+    knex.select().from("loginInfo").where({ id: userId }).then((user) => {
+        if (user.length === 0) {
+            // If the user is not found, render the profile page with null value for theLogin
+            res.render("userprofile", { theLogin: null });
+            return;
+        }
+        // Render the profile page with retrieved user data in theLogin
+        res.render("userprofile", { theLogin: user[0] });
     }).catch((error) => {
-        // Handle errors if any while fetching data
         console.error("Error fetching user data:", error);
         res.status(500).send("Error fetching user data");
     });
